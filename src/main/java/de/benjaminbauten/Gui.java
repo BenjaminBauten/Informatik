14272f19efed4690f7958b316fb81b6403ad3078
Benjamin Bauten <BenjaminBauten@web.de>
2022-08-26 22:13:30 +0200

    Zweiten Button "zeichne Baum" hinzugefügt. Problem: Die Buttons werden nicht richtig dargestellt.

--- src/main/java/de/benjaminbauten/Gui.java
+++ src/main/java/de/benjaminbauten/Gui.java
@@ -13,12 +13,12 @@ public class Gui {
     public void mainframe(EKlasse programm) {
 
         JFrame frame = new JFrame();
-        frame.setSize(500, 500);
+        frame.setSize(300, 150);
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JButton button = new JButton("Zeichne Haus");
-        button.addActionListener(e -> {
+        JButton zeichneHausButton = new JButton("Zeichne Haus");
+        zeichneHausButton.addActionListener(e -> {
             JTextField x = new JTextField(10);
             JTextField y = new JTextField(10);
             JTextField r = new JTextField(10);
@@ -37,8 +37,36 @@ public class Gui {
             }
         });
 
-        frame.getContentPane().add(button);
+        JButton zeichneBaumButton = new JButton("zeichne Baum");
+        zeichneBaumButton.addActionListener(e -> {
+            JTextField x = new JTextField(10);
+            JTextField y = new JTextField(10);
+            JTextField r = new JTextField(10);
+
+            JPanel dialog = new JPanel();
+            dialog.add(new JLabel("x"));
+            dialog.add(x);
+            dialog.add(new JLabel("y"));
+            dialog.add(y);
+            dialog.add(new JLabel("rotation"));
+            dialog.add(r);
+
+            int result = JOptionPane.showConfirmDialog(null, dialog, "Postion wählen", JOptionPane.OK_CANCEL_OPTION);
+            if (result == JOptionPane.OK_OPTION) {
+                programm.zeichneWeihnachtsbaum(Integer.parseInt(x.getText()), Integer.parseInt(y.getText()), Integer.parseInt(r.getText()));
+            }
+        });
+
+            zeichneHausButton.setBounds(0,0,100,50);
+        zeichneBaumButton.setBounds(0,0,100,50);
+            frame.add(zeichneHausButton);
+            frame.add(zeichneBaumButton);
         frame.setVisible(true);
+
+//            frame.getContentPane().add(zeichneHausButton);
+//            frame.getContentPane().add(zeichneBaumButton);
+//            frame.getContentPane().add(zeichneBaumButton);
+//        frame.setVisible(true);
 
 
     }
